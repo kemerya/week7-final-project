ddf1eed5c8a113dfb7fd8926e4c6e1c6df43ce92
Kemerya zeynu <123075499+kemerya@users.noreply.github.com>
2024-08-26 23:02:26 +0300

    apiKey-updated

--- src/index.js
+++ src/index.js
@@ -40,7 +40,8 @@ function refreshWeather(response) {
   }
   
   function searchCity(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+ 
+    let apiKey = "370o20fdt59b9c215a8f4fded1ebaa6c";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
   }
